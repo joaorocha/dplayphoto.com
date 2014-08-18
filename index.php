73eb137dbe29041f6d0acd557cd0b6da0b073dd3
joao.rochamelo1993@gmail.com <joao.rochamelo1993@gmail.com>
2014-08-18 13:35:59 -0300

    Início

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dplay - Home</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>        
+        <!-- Home -->
+        <div id="home" class="panel">
+            <div class="content">
+                <div class="home-content">
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <a href="#">
+                                <div class="caption"><p>Teste</p></div>
+                                <img src="img/1.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/3.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/6.jpg" class="img-responsive"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/9.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/10.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/5.jpg" class="img-responsive"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/7.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/4.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <a href="#"><img src="img/8.jpg" class="img-responsive"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <img src="img/1.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/3.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/6.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <img src="img/1.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/3.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/6.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <img src="img/1.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/3.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/6.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 no-padding">
+                            <img src="img/1.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/3.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <img src="img/6.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Home -->
+        
+        <!-- Portfolio -->
+        <div id="portfolio" class="panel">
+            <div class="content">
+                <h2>Portfolio</h2>
+                <p>Some really nice portfolio shots:</p>
+                <ul id="works">
+                    <li><a href="http://dribbble.com/shots/394177-Sheriff-Woody"><img src="images/portfolio_01.jpeg" width="250"></a></li>
+                    <li><a href="http://dribbble.com/shots/392403-Spaceman"><img src="images/portfolio_02.jpeg" width="250"></a></li>
+                    <li><a href="http://dribbble.com/shots/388799-Harvey-Birdman"><img src="images/portfolio_03.jpeg" width="250"></a></li>
+                </ul>
+                <p class="footnote">Dribbble shots by <a href="http://dribbble.com/stuntman">Matt Kaufenberg</a>.</p>
+            </div>
+        </div>
+        <!-- /Portfolio -->
+        
+        <!-- About -->
+        <div id="about" class="panel">
+            <div class="content">
+                <h2>About</h2>
+                <p>It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+                <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen.</p>
+                <p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+            </div>
+        </div>
+        <!-- /About -->
+        
+        <!-- Contact -->
+        <div id="contact" class="panel">
+            <div class="content">
+                <h2>Contact</h2>
+                <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
+                <p>Pityful a rethoric question ran over her cheek, then she continued her way. On her way she met a copy.</p>
+                <form id="form">
+                    <p><label>Your Name</label><input type="text" /></p>
+                    <p><label>Your Email</label><input type="text" /></p>
+                    <p><label>Your Message</label><textarea></textarea></p>
+                </form>
+            </div>
+        </div>
+        <!-- /Contact -->
+
+        <!-- Header with Navigation -->
+        <div id="header">
+            <img src="img/dplay-logo.jpg" id="logo1">
+            <ul id="navigation">
+                <li><a id="link-home" href="#home">Home</a></li>
+                <li><a id="link-portfolio" href="#portfolio">Portfolio</a></li>
+                <li><a id="link-about" href="#about">About Me</a></li>
+                <li><a id="link-contact" href="#contact">Contact</a></li>
+            </ul>
+            <div class="redes">
+                <a href=""><img src="img/icon-facebook.png"></a>
+                <a href=""><img src="img/icon-instagram.png"></a>
+                <a href=""><img src="img/icon-google.png"></a>
+            </div>
+            <div class="grave">
+                <p>Desenvolvido por Graveyard Studios</p>
+            </div>
+        </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
